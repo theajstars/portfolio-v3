54b8591ae717176d00b01394b9d9b29e8d0fc46f
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2025-02-17 23:42:26 +0100

    :ambulance: Worked on `Jumbotron`

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -3,6 +3,10 @@ import AvatarImage from "../../assets/img/avatar.jpg";
 export const Colors = {
   BlackPrimary: "var(--black-primary)",
   GreenPrimary: "var(--green-primary)",
+  WhitePrimary: "var(--white-primary)",
+  BodyPrimary: "var(--body-primary)",
+  GrayPrimary: "var(--gray-primary)",
+  WhatsappPrimary: "var(--whatsapp-primary)",
 };
 export const Images = {
   AvatarImage,
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,20 +2,28 @@ import React from "react";
 import { Container } from "@mui/material";
 
 import "./styles.scss";
-import { Button, HoverCard, Text } from "@mantine/core";
-import { Images } from "../../lib/data";
+import {
+  ActionIcon,
+  Button,
+  Card,
+  HoverCard,
+  Paper,
+  Text,
+} from "@mantine/core";
+import { Colors, Images } from "../../lib/data";
+import { IconBrandWhatsapp, IconMail } from "@tabler/icons-react";
 const Home: React.FC = () => {
   return (
     <Container maxWidth="lg">
       <div className="home-container">
         <div className="jumbotron flex-col">
           <div className="flex-row align-center row">
-            <Text className="text">Hi, I'm</Text>{" "}
+            <Text className="text">Hi, I'm</Text>
             <img src={Images.AvatarImage} alt="" className="avatar" />
-            <Text className="text">Ajiboye Oluwaferanmi</Text>
+            <Text className="text">Oluwaferanmi!</Text>
           </div>
           <div className="flex-row align-center row">
-            <Text className="text">I'm a</Text>
+            <Text className="text">I'm a</Text>&nbsp;
             <Text className="text">Software Developer</Text>
           </div>
           <HoverCard>
@@ -24,11 +32,29 @@ const Home: React.FC = () => {
                 variant="outline"
                 className="btn"
                 leftSection={<span className="loader"></span>}
+                color={Colors.GrayPrimary}
+                c={Colors.BlackPrimary}
+                fw={400}
+                fz={17}
               >
                 Open to Work
               </Button>
             </HoverCard.Target>
+            <HoverCard.Dropdown>
+              <div className="flex-row align-center">
+                <ActionIcon size={50} variant="subtle">
+                  <IconBrandWhatsapp size={30} color={Colors.WhatsappPrimary} />
+                </ActionIcon>
+                <ActionIcon size={50} variant="subtle">
+                  <IconMail size={30} />
+                </ActionIcon>
+              </div>
+            </HoverCard.Dropdown>
           </HoverCard>
+          <Text fw={500} fz={20} mt={20}>
+            Feel free to explore my portfolio and reach out. I'd love to
+            connect!
+          </Text>
         </div>
       </div>
     </Container>
